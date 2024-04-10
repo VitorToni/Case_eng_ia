4113ff40ef18d1f1f3efb7408846bbbfd5819c83
Vitor Toni <vitortonidocampo2@yahoo.com.br>
2024-04-09 22:35:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+from instalar_dependencias import *
+
+if __name__ == "__main__":
+    instalar_dependencias()
+    print("Instalação de dependências concluída. Aguardando 3 segundos...")
